5c8ccd49607cacec371c1f3e8704077b5effab48
korxz <roksilic.net@gmail.com>
2018-03-18 23:37:04 +0100

    Added method for calculating distance between 2 places

--- app/Krajevne_lastnosti.php
+++ app/Krajevne_lastnosti.php
@@ -3,13 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Nesreca;
 
 class Krajevne_lastnosti extends Model
 {
     protected $table = 'krajevne_lastnosti';
+    private $earthR = 637100;
 
     protected $fillable = [
-        'odsek', 'sifra_oznaka', 'kraj_nesrece', 'hisna_st', 'prizorisce', 'vzrok', 'tip',
+        'id_nesrece', 'odsek', 'sifra_oznaka', 'kraj_nesrece', 'hisna_st', 'prizorisce', 'vzrok', 'tip',
     ];
 
     public function nesreca()
@@ -20,5 +22,21 @@ class Krajevne_lastnosti extends Model
     public function getSection()
     {
         return $this->odsek;
+    }
+
+    public function near($x, $y)
+    {
+        $accident = Nesreca::where("id_nesrece", "=", $this->id_nesrece)->get();
+        
+        $latFrom = deg2rad((float) $x);
+        $lonFrom = deg2rad((float) $y);
+
+        $latTo = deg2rad($accident[0]->x);
+        $lonTo = deg2rad($accident[0]->y);
+
+        $latDelta = $latFrom - $latTo;
+        $lonDelta = $lonFrom - $lonTo;
+        $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) + cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+        return $angle * $this->earthR;
     }
 }
